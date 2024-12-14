a6626cae960c347701917f5d866517cddd8abaf1
Ikeora <ekene.ikeora@gmail.com>
2024-12-14 16:40:57 -0700

    Modified the html templates

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 
@@ -12,7 +11,7 @@ app=application
 
 # Route for a home page
 
-@app.route('/')
+@app.route('/') #base url of web app
 def index():
     return render_template('index.html')
 
